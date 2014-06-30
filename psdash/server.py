22551ebb169c15a00834d0d86caba52c27699179
DjvuLee <lihu723@gmail.com>
2014-06-30 21:44:30 +0800

    add log in the server

--- psdash/server.py
+++ psdash/server.py
@@ -16,7 +16,47 @@ import time
 logs = Logs()
 net_io_counters = NetIOCounters()
 
-def start_background_worker(sleep_time=3):
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="psdash %s - system information web dashboard" % "0.3.0"
+    )
+    parser.add_argument(
+        "-l", "--log",
+        action="append",
+        dest="logs",
+        default=[],
+        metavar="path",
+        help="log files to make available for psdash. Patterns (e.g. /var/log/**/*.log) are supported. "
+             "This option can be used multiple times."
+    )
+    parser.add_argument(
+        "-b", "--bind",
+        action="store",
+        dest="bind_host",
+        default="0.0.0.0",
+        metavar="host",
+        help="host to bind to. Defaults to 0.0.0.0 (all interfaces)."
+    )
+    parser.add_argument(
+        "-p", "--port",
+        action="store",
+        type=int,
+        dest="port",
+        default=5000,
+        metavar="port",
+        help="port to listen on. Defaults to 5000."
+    )
+    parser.add_argument(
+        "-d", "--debug",
+        action="store_true",
+        dest="debug",
+        help="enables debug mode."
+    )
+
+    return parser.parse_args()
+
+
+def start_background_worker(args, sleep_time=3):
     def work():
         update_logs_interval = 60
         i = update_logs_interval
@@ -25,7 +65,7 @@ def start_background_worker(sleep_time=3):
 
             # update the list of available logs every minute
             if update_logs_interval <= 0:
-                #logs.add_patterns(args.logs)
+                logs.add_patterns(args.logs)
                 i = update_logs_interval
             i -= sleep_time
 
@@ -36,12 +76,12 @@ def start_background_worker(sleep_time=3):
     t.start()
 
 def main():
-    #setup_logging()
+    setup_logging()
 
-    #logger.info("Starting clusterdash v0.1.0")
-    #locale.setlocale(locale.LC_ALL, "")
+    logger.info("Starting server ...")
+    locale.setlocale(locale.LC_ALL, "")
 
-    start_background_worker()
+    start_background_worker(args)
     wrapService = WrapService(net_io_counters)
     s = ThreadedServer(wrapService, port=5050,  protocol_config={"allow_public_attrs":True})
     s.start()
